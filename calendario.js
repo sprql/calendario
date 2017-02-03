594d66a9812898fdd151434dfc3b5cf6acb28e18
Alexander Obukhov <github@sprql.space>
2017-02-03 08:01:42 +0300

    Improvements

--- calendario.js
+++ calendario.js
@@ -1,8 +1,21 @@
+var fontFamily = 'Ubuntu';
+
 function $id(id) { return document.getElementById(id); }
 
 Date.prototype.getWeek = function() {
-    var onejan = new Date(this.getFullYear(), 0, 1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    var dayNumber = (this.getDay() + 6) % 7;
+
+    var target = new Date(this.valueOf());
+    target.setDate(target.getDate() - dayNumber + 3);
+
+    var firstThursday = target.valueOf();
+
+    target.setMonth(0, 1);
+    if (target.getDay() != 4) {
+        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+    }
+
+    return 1 + Math.ceil((firstThursday - target) / (7 * 24 * 3600 * 1000));
 }
 
 function HighlightDays(days) {
@@ -46,20 +59,20 @@ function dayCell(ctx, text, className) {
 
 function sprintIdCell(ctx, date) {
     var sprintId = date.getWeek();
-    sprintId = Math.floor((sprintId/2)) - 1;
-    var sprintAlphaId = alpha[sprintId].toUpperCase();
+    var sprintHalfId = Math.floor((sprintId/2)) - 1;
+    var sprintAlphaId = alpha[sprintHalfId];
 
     var cr = 5;
     var dv = ctx.group();
     dv.rect(wc, wc);
 
-    var t = dv.text(sprintAlphaId);
+    var t = dv.text(sprintId + '.' + sprintAlphaId);
     t.move(wc / 2, 0);
     t.font({
         family:   fontFamily,
-        size:     baseFontSize * 0.5,
+        size:     baseFontSize * 0.3,
         anchor:   'middle',
-        leading:  '2.7em'
+        leading:  '4em'
     });
 
     dv.attr({ class: 'sprint-id' });
@@ -69,6 +82,7 @@ function sprintIdCell(ctx, date) {
 
 function renderMonth(ctx, year, month, highlightDays) {
     var monthFullNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var dayFullNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     var dayShortNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
     var hlDays = (highlightDays) ? new HighlightDays(highlightDays) : null;
@@ -97,22 +111,25 @@ function renderMonth(ctx, year, month, highlightDays) {
 
     for (var d = 0; d < 7; d++) {
         var className = 'day day-names' + ((d >= 5) ? ' day-holyday' : '');
-        var dv = dayCell(daysMatrix, dayShortNames[d], className);
+        var dv = dayCell(daysMatrix, dayFullNames[d], className);
         dv.move(wc * d, yk * wc);
-        dv.get(1).font({ size: baseFontSize / 2, leading: '3em' });
+        dv.get(1).font({ size: baseFontSize / 2.75, leading: '3em' });
     }
 
     yk++;
 
     var weekDay = cweek(new Date(month + '/01/' + year));
 
-    for (var d = 0; d < (weekDay - 1); d++) {
+    var date = null;
+
+    for (var d = 0; d < weekDay; d++) {
         var dv = dayCell(daysMatrix, null, 'day of-prev-month');
         dv.move(d * wc, yk * wc);
     }
 
+
     for (var d = 1; d < 32; d++) {
-        var date = new Date(month + '/' + d + '/' + year);
+        date = new Date(month + '/' + d + '/' + year);
         if ((date.getMonth() + 1) > month) {
             break;
         }
@@ -143,12 +160,10 @@ function renderMonth(ctx, year, month, highlightDays) {
         dv.move(wc * d, wc * yk);
     }
 
-
     daysMatrix.move(wc, h - (yk + 2) * wc);
 }
 
 var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var fontFamily = 'Helvetica Neue';
 
 var svg = document.getElementsByTagName('svg')[0]
 
@@ -169,4 +184,5 @@ var ctx = s.group();
 var image = ctx.image('image.jpg', w, 4 * wc);
 image.move(0, wc/2);
 
-renderMonth(ctx, 2017, 2);
+var currentDate = new Date();
+renderMonth(ctx, currentDate.getFullYear(), currentDate.getMonth() + 1);
