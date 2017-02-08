b1356ee59392f1ba8f82ff6fdc284d69c76ad568
Alexander Obukhov <github@sprql.space>
2017-02-08 10:21:18 +0300

    Improvements

--- calendario.js
+++ calendario.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'z+'];
 const monthFullNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const dayFullNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -18,12 +20,12 @@ const imageURL = 'image.jpg';
 function $id(id) { return document.getElementById(id); }
 
 Date.prototype.getWeek = function() {
-    var dayNumber = (this.getDay() + 6) % 7;
+    let dayNumber = (this.getDay() + 6) % 7;
 
-    var target = new Date(this.valueOf());
+    let target = new Date(this.valueOf());
     target.setDate(target.getDate() - dayNumber + 3);
 
-    var firstThursday = target.valueOf();
+    let firstThursday = target.valueOf();
 
     target.setMonth(0, 1);
     if (target.getDay() != 4) {
@@ -34,18 +36,16 @@ Date.prototype.getWeek = function() {
 }
 
 Date.prototype.getWeekDay = function() {
-    var day = this.getDay();
-    day--;
-    if (day == -1) { day = 6 }
-    return day;
+    let day = this.getDay();
+    return (day == 0) ? 6 : day - 1;
 }
 
 
 function dayNameCell(text, className) {
-    var dv = new SVG.G();
+    let dv = new SVG.G();
     dv.rect(wc, wc);
 
-    var t = dv.text(text)
+    let t = dv.text(text)
     t.move(wc / 2, 0);
     t.font({
         family:   fontFamily,
@@ -60,11 +60,11 @@ function dayNameCell(text, className) {
 }
 
 function dayCell(ctx, text, className) {
-    var cr = 5;
-    var dv = ctx.group();
+    let cr = 5;
+    let dv = ctx.group();
     dv.rect(wc, wc);
     if (text) {
-        var t = dv.text(text)
+        let t = dv.text(text)
         t.move(wc / 2, 0);
         t.font({
             family:   fontFamily,
@@ -85,15 +85,15 @@ function dayCell(ctx, text, className) {
 }
 
 function sprintIdCell(ctx, date) {
-    var sprintId = date.getWeek();
-    var sprintHalfId = Math.ceil((sprintId/2)) - 1;
-    var sprintAlphaId = alpha[sprintHalfId];
+    let sprintId = date.getWeek();
+    let sprintHalfId = Math.ceil((sprintId/2)) - 1;
+    let sprintAlphaId = alpha[sprintHalfId];
 
-    var cr = 5;
-    var dv = ctx.group();
+    let cr = 5;
+    let dv = ctx.group();
     dv.rect(wc, wc);
 
-    var t = dv.text(sprintId + ' ' + sprintAlphaId);
+    let t = dv.text(sprintId + ' ' + sprintAlphaId);
     t.move(wc / 2, 0);
     t.font({
         family:   fontFamily,
@@ -108,16 +108,16 @@ function sprintIdCell(ctx, date) {
 }
 
 function renderMonth(year, month, highlightDays) {
-    var table = new SVG.G();
-    var yk = 0;
+    let table = new SVG.G();
+    let yk = 0;
 
-    var monthBlock = table.group();
-    var daysMatrix = monthBlock.group();
-    var g = daysMatrix.group();
+    let monthBlock = table.group();
+    let daysMatrix = monthBlock.group();
+    let g = daysMatrix.group();
 
     g.attr({ class: 'month-name' });
     g.move(0, yk * wc);
-    var t = g.text(monthFullNames[month - 1]);
+    let t = g.text(monthFullNames[month - 1]);
     t.move(w - 2*wc, 0);
     t.font({
         family:   fontFamily,
@@ -128,41 +128,41 @@ function renderMonth(year, month, highlightDays) {
 
     yk++;
 
-    for (var d = 0; d < 7; d++) {
-        var className = 'day day-names' + ((d >= 5) ? ' day-holyday' : '');
-        var dv = dayNameCell(dayFullNames[d], className);
+    for (let d = 0; d < 7; d++) {
+        let className = 'day day-names' + ((d >= 5) ? ' day-holyday' : '');
+        let dv = dayNameCell(dayFullNames[d], className);
         dv.move(wc * d, yk * wc);
         daysMatrix.add(dv);
     }
 
 
-    var date = new Date(month + '/' + 1 + '/' + year);
-    var firstWeekDayOfMonth = date.getWeekDay();
+    let date = new Date(month + '/' + 1 + '/' + year);
+    let firstWeekDayOfMonth = date.getWeekDay();
 
     if (firstWeekDayOfMonth > 0) {
         yk++;
-        var sprintCell = sprintIdCell(daysMatrix, date);
+        let sprintCell = sprintIdCell(daysMatrix, date);
         sprintCell.move(-wc, wc * yk);
     }
 
     date.setDate(date.getDate() - firstWeekDayOfMonth);
-    for (var d = 0; d < firstWeekDayOfMonth; d++) {
-        var dv = dayCell(daysMatrix, date.getDate().toString(), 'day of-prev-month');
+    for (let d = 0; d < firstWeekDayOfMonth; d++) {
+        let dv = dayCell(daysMatrix, date.getDate().toString(), 'day of-prev-month');
         dv.move(d * wc, yk * wc);
         date.setDate(date.getDate() + 1);
     }
 
-    for (var d = 1; d < 32; d++) {
-        weekDay = date.getWeekDay();
+    for (let d = 1; d < 32; d++) {
+        let weekDay = date.getWeekDay();
         if (weekDay == 0) {
             yk++;
-            var sprintCell = sprintIdCell(daysMatrix, date);
+            let sprintCell = sprintIdCell(daysMatrix, date);
             sprintCell.move(-wc, wc * yk);
         }
 
-        var className = 'day';
+        let className = 'day';
         if (weekDay > 4) { className += ' day-holyday'; }
-        var dv = dayCell(daysMatrix, d.toString(), className);
+        let dv = dayCell(daysMatrix, d.toString(), className);
         dv.move(wc * weekDay, wc * yk);
 
         date.setDate(date.getDate() + 1);
@@ -171,18 +171,21 @@ function renderMonth(year, month, highlightDays) {
         }
     }
 
-    for (var d = (weekDay + 1); d < 7; d++) {
-        var className = 'day of-next-month';
-        if (d > 4) { className += ' day-holyday'; }
-        var dv = dayCell(daysMatrix, date.getDate().toString(), className);
-        dv.move(wc * d, wc * yk);
-        date.setDate(date.getDate() + 1);
+    let lastWeekDayOfMonth = date.getWeekDay();
+    if (lastWeekDayOfMonth > 0) {
+        for (let d = lastWeekDayOfMonth; d < 7; d++) {
+            let className = 'day of-next-month';
+            if (d > 4) { className += ' day-holyday'; }
+            let dv = dayCell(daysMatrix, date.getDate().toString(), className);
+            dv.move(wc * d, wc * yk);
+            date.setDate(date.getDate() + 1);
+        }
     }
 
     let tableHeight = (yk + 1.5) * wc;
     daysMatrix.move(wc, h - tableHeight);
 
-    var image = ctx.image(imageURL, w, h - tableHeight);
+    let image = table.image(imageURL, w, h - tableHeight);
     image.move(0, 0);
     image.attr({ preserveAspectRatio: 'xMaxYMid slice' });
 
@@ -190,13 +193,13 @@ function renderMonth(year, month, highlightDays) {
 }
 
 
-var svg = document.getElementsByTagName('svg')[0]
-var s = SVG(svg);
+let svg = document.getElementsByTagName('svg')[0];
+let s = SVG(svg);
 
 s.viewbox(0, 0, w, h);
 
-var ctx = s.group();
+let ctx = s.group();
 
-var currentDate = new Date();
-var month = renderMonth(currentDate.getFullYear(), currentDate.getMonth() + 1);
+let currentDate = new Date();
+let month = renderMonth(currentDate.getFullYear(), currentDate.getMonth() + 1);
 ctx.add(month);
